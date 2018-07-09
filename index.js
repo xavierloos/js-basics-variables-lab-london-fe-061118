dff4b9d231b12e6f5f495ab8a0fafc76c6e168a8
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-09 19:57:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});
+const companyName='Scuber';
+var mostProfitableNeighborhood= 'Chelsea'
+var companyCeo='Susan Smith'
+
