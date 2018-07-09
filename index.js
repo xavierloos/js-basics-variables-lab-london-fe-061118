9cded7fa9ebe12e122fc5c81f25410b27ebadb0c
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-09 19:56:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ it('is set as Scuber', function () {
   expect(companyName).to.equal('Scuber');
 });
 const companyName='Scuber';
-var Chelsea ="mostProfitableNeighborhood"
+var mostProfitableNeighborhood=
 
